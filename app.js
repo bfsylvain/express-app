2f2ee5ddfa96a50261f8721af850c7a1c383371d
bfsylvain <sylvainbonnaure@hotmail.com>
2023-11-27 10:03:04 +0100

    first commit before quest 01.1

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,  
+},
+];
+
+// const findMovies = (req, res) => {
+//     if (req.url === "/") {
+//         res.send("Welcome to my favourite movie list !")
+//     }else if(req.url === "/api/movies") {
+//         res.status(200).json(movies)
+//     } else {
+//         let wantedMovie = movies.find(movie => movie.id === req.url.params.number)
+//         if(wantedMovie) {
+//             res.status(200).json(wantedMovie);
+//         } else {
+//             res.status(404).send("ayoy")
+//         }
+//     }
+// };
+const welcomeMsg = (req, res) => {
+    res.send("Welcome to my favourite movie list !")
+}
+const moviesList = (req, res) => {
+    res.status(200).json(movies)
+}
+const findMovies = (req, res) => {
+    let wantedMovie = movies.find((movie) => movie.id === parseInt(req.params.id))
+    wantedMovie ? res.status(200).json(wantedMovie.title) : res.status(404).send("Not Found")       
+}
+
+app.get("/", welcomeMsg)
+app.get("/api/movies", moviesList);
+app.get("/api/movies/:id", findMovies)
+
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("ayaya something bad happened");
+  } else {
+    console.log(`Servier is listening on ${port}`);
+  }
+});
